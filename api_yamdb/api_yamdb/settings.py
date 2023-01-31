900facddcde7ba3219c7f7e39db5236a7758370f
gusevskiy <vlgu@mail.ru>
2023-01-31 15:59:03 +0300

    create models reviews

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,3 +101,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+
+AUTH_USER_MODEL = 'users.User'
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# User = get_user_model()
+
+class User(AbstractUser):
+    bio = models.TextField(
+    'Биография',
+    blank=True,
+    )
+
+
+# id,title_id,text,author,score,pub_date
+class Review(models.Model):
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Сomposition'
+    )
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Author'
+    )
+    text = models.TimeField()
+    score = models.IntegerField(
+        verbose_name='Evaluation'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='date of publication'
+    )
+    def __str__(self):
+        return self.text

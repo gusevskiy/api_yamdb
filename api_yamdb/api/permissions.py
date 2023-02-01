a5ea913bf3d7b25465e1b9757d307c5ea85cd4df
Тимофей Сухов <tsukhov77@gmail.com>
2023-02-01 18:27:41 +0300

    Added IsUserAuthorOrModeratorOrReadOnly permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,5 +12,32 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.user.is_anonymous:
             return False
         is_superuser = request.user.is_superuser
-        is_admin = request.user.level == "admin"
+        is_admin = request.user.role == "admin"
         return is_superuser or is_admin
+
+
+class IsUserAuthorOrModeratorOrReadOnly(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь автором поста или модератором.
+    """
+    def has_permission(self, request, view):
+        is_safe = request.method in permissions.SAFE_METHODS
+        if is_safe:
+            return True
+        if request.user.is_anonymous:
+            return False
+        if request.user.role == "moderator":
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        is_safe = request.method in permissions.SAFE_METHODS
+        if is_safe:
+            return True
+        if request.user.is_anonymous:
+            return False
+        if request.user.role == "moderator":
+            return True
+        if obj.author == request.user:
+            return True
+        return False
